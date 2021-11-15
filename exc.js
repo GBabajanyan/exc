0228c00370d4ccc962eb1438c09092f2100db6eb
George <george.babajanyan@gmail.com>
2021-11-15 21:01:10 +0400

    first commit

---
+++ exc.js
@@ -0,0 +1,35 @@
+function getCombs(arr) {
+    if (arr.length === 0) return [[]];
+    const a = arr[0]
+    const rest = arr.slice(1)
+
+    const withoutFirst = getCombs(rest)
+    const withFirst = []
+    for (let i = 0; i < withoutFirst.length; i++) {
+        const combination = [...withoutFirst[i], a]
+        withFirst.push(combination)
+    }
+    return [...withoutFirst, ...withFirst]
+}
+
+function getPerms(arr) {
+    if (arr.length === 0) return [[]];
+    const a = arr[0]
+    const rest = arr.slice(1)
+
+    const withoutFirst = getPerms(rest)
+    const allPerms = []
+    withoutFirst.forEach(el => {
+        for (let j = 0; j <= el.length; j++) {
+            const perm = [...el.slice(0, j), a, ...el.slice(j)]
+            allPerms.push(perm)
+        }
+    });
+    return allPerms
+}
+function getPosibilites(params) {
+    let ka = []
+    let a = getCombs(params).map(el => ka.push(...getPerms(el)))
+    return ka
+}
+console.log(getPosibilites(['a','b']));
